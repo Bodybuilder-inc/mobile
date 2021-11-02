d83e4f0e3c87dddcd6982fc44d9387daad8dcad6
clertonf <clerton.filho7@gmail.com>
2021-11-01 22:52:55 -0300

    pre-configured global themes

---
+++ src/styles/theme.ts
@@ -0,0 +1,28 @@
+export default {
+  colors: {
+    background_primary: "#1C1C1C",
+    background_secondary: "#FFFFFF",
+
+    background_dark: "#000000",
+
+    green: "#006536",
+
+    text: "#7A7A80",
+    text_dark: "#000000",
+    text_detail: "#AEAEB3",
+    title: "#47474D",
+
+    line: "#EBEBF0",
+
+    error: "#DC1637",
+    success: "#03B252",
+
+    shape: "#E1E1E8",
+    shape_dark: "#29292E",
+  },
+  fonts: {
+    primary_400: "Inter_400Regular",
+    primary_600: "Inter_600SemiBold",
+    primary_700: "Inter_700Bold",
+  },
+};
