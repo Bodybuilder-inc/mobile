1bab8d65313c237a0cd63ce0ebb2383213df54d2
clertonf <clerton.filho7@gmail.com>
2021-11-01 22:50:04 -0300

    added fonts

--- App.tsx
+++ App.tsx
@@ -1,7 +1,34 @@
 import React from "react";
 
-import { Home } from "./src/screens/Home";
+import { StatusBar } from "expo-status-bar";
+import { ThemeProvider } from "styled-components";
+import AppLoading from "expo-app-loading";
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+} from "@expo-google-fonts/inter";
+
+import { Login } from "./src/screens/Login";
+
+import theme from "./src/styles/theme";
 
 export default function App() {
-  return <Home />;
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <StatusBar backgroundColor="transparent" translucent />
+      <Login />
+    </ThemeProvider>
+  );
 }
